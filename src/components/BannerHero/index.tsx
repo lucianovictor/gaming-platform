4300ccb7893e58ea740af4fadb813a9555574878
luciano victor <lucianocarvallho7@gmail.com>
2022-07-12 12:27:39 -0300

    feat: update section bannerHero

--- src/components/BannerHero/index.tsx
+++ src/components/BannerHero/index.tsx
@@ -2,12 +2,12 @@ import styles from './styles.module.scss'
 
 export const BannerHero = () => {
   return (
-    <>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <div className={styles.content}>
         <h1>Currently Trending Games</h1>
-        <button type="button">SEE ALL</button>
+        <button>SEE ALL</button>
       </div>
       <div className={styles.images}></div>
-    </>
+    </div>
   )
 }
